8b374f96357346230001bc5caffb36a1b3894e73
Chava Sobreyra <chava.sobreyra@gmail.com>
2018-10-03 17:55:08 -0400

    added notes for part 0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,32 +41,32 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop()
-  private msg!: string
+  private msg!: string;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3 
-  margin 40px 0 0
+h1 {
+  margin: 40px 0 0;
+}
 
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-ul 
-  list-style-type none
-  padding 0
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-
-li 
-  display inline-block
-  margin 0 10px
-
-
-a 
-  color #42b983
-
+a {
+  color: #42b983;
+}
 </style>
--- src/store.ts
+++ src/store.ts
@@ -4,14 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
+  state: {},
 
-  mutations: {
+  mutations: {},
 
-  },
-
-  actions: {
-
-  },
+  actions: {},
 })
