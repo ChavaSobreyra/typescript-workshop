fff684454dc3bede6b0ac1c9e7ca28a3c5bca9ab
Eliurkis <eliurkis@gmail.com>
2018-10-04 21:38:08 -0400

    :pizza: Add pizza ordering system

--- pizza/index.ts
+++ pizza/index.ts
@@ -1,26 +1,5 @@
 import {Pizza, DeliveryInstructions, Receipt, Pizzeria} from './index.types'
 
-abstract class Pizzeriaa {
-  private name: string
-  private phoneNumber: number
-  private offersDelivery: boolean
-  private readonly price: number
-  private readonly driver: string
-
-  protected constructor(name: string, phoneNumber: number, offersDelivery: boolean, price: number, driver: string) {
-    this.name = name
-    this.phoneNumber = phoneNumber
-    this.offersDelivery = offersDelivery
-    this.price = price
-    this.driver = driver
-  }
-
-  abstract placeOrder(pizza: Pizza): {
-    price: number,
-    driver: string
-  }
-}
-
 function orderPizza(pizza: Pizza, provider: Pizzeria, deliveryInstructions: DeliveryInstructions): Receipt {
   const placeOrderSummary = provider.placeOrder(pizza)
 
